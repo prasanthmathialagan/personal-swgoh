6b21b89a3d1d6230464ab72c75dbd746ba3a909b
Prasanth Mathialagan <prasanthmathialagan@gmail.com>
2017-10-13 20:17:31 -0700

    Support for querying greeter about speed

--- src/main/resources/bot.js
+++ src/main/resources/bot.js
@@ -49,6 +49,17 @@ toons_con.connect(function(err) {
     toons_con.end();
 });
 
+function find_closest_match_toon(all_toons, input) {
+	input = input.replace("-", " ");
+	for (i in all_toons) {
+		var t = all_toons[i];
+		if (t.toLowerCase().indexOf(input.toLowerCase()) >= 0) {
+			return t;
+		}
+	}
+	return "";
+}
+
 var members_con = get_db_connection();
 members = "";
 members_list = [];
@@ -72,6 +83,60 @@ members_con.connect(function(err) {
 
     members_con.end();
 });
+
+function find_closest_match_member(all_members, input) {
+	input = input.replace("'s", "");
+	for (i in all_members) {
+		var t = all_members[i];
+		if (t.toLowerCase().indexOf(input.toLowerCase()) >= 0) {
+			return t;
+		}
+	}
+	return "";
+}
+
+function send_speed(member, toon, bot, channelID) {
+	var con = get_db_connection();
+	con.connect(function(err) {
+		var speed = -1;
+	  	if (err) {
+			console.log(err);
+			bot.sendMessage({
+				to: channelID,
+				message: 'Internal error. Try again after some time!'
+			});
+	  	} else {
+			con.query("select gt.speed from Users u INNER JOIN GuildToons gt ON u.id = gt.userId INNER JOIN Toons t ON gt.toonId = t.id AND t.name = ? AND u.name = ?", [toon, member], function (err, result, fields) {
+				if (err) {
+					console.log(err);
+					bot.sendMessage({
+						to: channelID,
+						message: 'Internal error. Try again after some time!'
+					});
+				} else {
+					if (result.length == 0) {
+						bot.sendMessage({
+							to: channelID,
+							message: 'No results found!'
+						});
+					} else {
+						for (i in result) {
+							var o = result[i];
+							speed = o['speed'];
+							bot.sendMessage({
+								to: channelID,
+								message: "Speed of " + member + "'s " + toon + " is " + speed
+							});
+							break;
+						}
+					}
+				}
+			});
+			con.end();
+		}
+	});
+
+}
 
 bot.on('ready', function (evt) {
     logger.info('Connected');
@@ -128,6 +193,7 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                 });
             break;
             case 'guild-member':
+            //
                 name = "";
                 for (i in args) {
                     var n = args[i];
@@ -139,12 +205,42 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                         }
                     }
                 }
+
+                console.log(name);
+                name_found = 1;
+                closest_match = 0;
                 if (name.length == 0) {
                     bot.sendMessage({
                         to: channelID,
                         message: 'Must supply a name. Usage: @greeter guild-member Celessa Laike'
                     });
-                } else {
+                } else if (members_list.indexOf(name) < 0) {
+					for (i in members_list) {
+						var t = members_list[i];
+						if (t.toLowerCase().indexOf(name.toLowerCase()) >= 0) {
+							console.log("Closest match to " + name + " is " + t);
+							name=t;
+							name_found = 1;
+							closest_match = 1;
+							break;
+						}
+					}
+                }
+
+                if (closest_match == 0) {
+                	name_found = 0;
+					bot.sendMessage({
+						to: channelID,
+						message: 'No results found for ' + name + '. Check the supplied name. Use @greeter members to get correct name'
+					});
+				} else {
+					bot.sendMessage({
+						to: channelID,
+						message: 'Closest match is ' + name + '.'
+					});
+				}
+
+                if (name_found == 1) {
                     var con = get_db_connection();
                     con.connect(function(err) {
                       if (err) {
@@ -154,7 +250,6 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                             message: 'Internal error. Try again after some time!'
                         });
                       } else {
-                            console.log(name);
                             con.query("select t.name, gt.star, gt.galacticPower, if(gt.speed = 0, 'N/A', speed) as speed from Users u INNER JOIN GuildToons gt ON u.id = gt.userId AND u.name=? INNER JOIN Toons t ON gt.toonId = t.id ORDER BY gt.star DESC, gt.galacticPower DESC", [name], function (err, result, fields) {
                                 if (err) {
                                     console.log(err);
@@ -164,10 +259,11 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                                     });
                                 } else {
                                     if (result.length == 0) {
-                                        bot.sendMessage({
-                                            to: channelID,
-                                            message: 'No results found for ' + name + '. Check the supplied name. Use !members to get correct name'
-                                        });
+                                    // This will not happen
+										bot.sendMessage({
+											to: channelID,
+											message: 'There is no player in the guild with the name ' + name + '.'
+										});
                                     } else {
                                         output = "Toon, star, GP, Speed\n----------------------\n"
                                         for (i in result) {
@@ -219,7 +315,7 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                         to: channelID,
                         message: 'Must supply a toon name. Usage: @greeter member-toon Ewok Scout'
                     });    
-                } else if (toons.indexOf(name) < 0) {
+                } else if (toons_list.indexOf(name) < 0) {
 					// Find the closest match
 					for (i in toons_list) {
 						var t = toons_list[i];
@@ -299,10 +395,110 @@ bot.on('message', function (user, userID, channelID, message, evt) {
             //
             break;
             default:
-            	bot.sendMessage({
-					to: channelID,
-					message: '“You will find only what you bring in.” - Yoda'
-				});
+            	player="";
+                toon="";
+				toon_found=0;
+				player_found=0;
+                for (i in args) {
+                	if (player_found == 1 && toon_found == 1) {
+						break;
+                	}
+
+					var n = args[i].trim().toLowerCase();
+					if (n === "is" || n === "and" || n === "the" || n === "of") {
+						continue;
+					}
+
+					if (n.length != 0) {
+						if (toon_found == 0) {
+							var t = find_closest_match_toon(toons_list, n);
+							if (t.length != 0) {
+								toon = t;
+								toon_found = 1;
+								continue;
+							}
+						}
+
+						if (player_found == 0) {
+							var t = find_closest_match_member(members_list, n);
+							if (t.length != 0) {
+								player = t;
+								player_found = 1;
+								continue;
+							}
+						}
+					}
+				}
+
+				msg = message.toLowerCase();
+
+				console.log(player);
+                console.log(toon);
+
+            	// Find the speed of a particular member's toon
+            	if (msg.indexOf("speed") >= 0) {
+            		if (player_found == 1 && toon_found == 1) {
+            			// Get the speed from database
+            			send_speed(player, toon, bot, channelID);
+            		} else if (player_found == 1 && toon_found == 0 ) {
+						bot.sendMessage({
+							to: channelID,
+							message: 'Could not find the specified toon for ' + player + "!!!"
+						});
+            		} else if (player_found == 0 && toon_found == 1 ) {
+						bot.sendMessage({
+							to: channelID,
+							message: toon + " of who??"
+						});
+					} else {
+						bot.sendMessage({
+							to: channelID,
+							message: 'Not enough information to find speed!!!'
+						});
+            		}
+            	}
+
+            	if (msg.indexOf("potency") >= 0) {
+					bot.sendMessage({
+						to: channelID,
+						message: 'My master has not taught me how to find potency!!!'
+					});
+				}
+
+				if (msg.indexOf("tenacity") >= 0) {
+					bot.sendMessage({
+						to: channelID,
+						message: 'My master has not taught me how to find tenacity!!!'
+					});
+				}
+
+				if (msg.indexOf("health") >= 0) {
+					bot.sendMessage({
+						to: channelID,
+						message: 'My master has not taught me how to find health!!!'
+					});
+				}
+
+				if (msg.indexOf("protection") >= 0) {
+					bot.sendMessage({
+						to: channelID,
+						message: 'My master has not taught me how to find protection!!!'
+					});
+				}
+
+				if (msg.indexOf("critical") >= 0 && msg.indexOf("chance") >= 0) {
+					bot.sendMessage({
+						to: channelID,
+						message: 'My master has not taught me how to find Critical Chance!!!'
+					});
+				}
+
+				if (msg.indexOf("critical") >= 0 && msg.indexOf("damage") >= 0) {
+					bot.sendMessage({
+						to: channelID,
+						message: 'My master has not taught me how to find Critical Damage!!!'
+					});
+				}
             // Just add any case commands if you want to..
          }
      }
